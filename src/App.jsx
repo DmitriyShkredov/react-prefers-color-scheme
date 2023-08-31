6c94ecfb24e683e17c2a59dfe86fcde8b37b0c00
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-08-31 11:27:48 +0300

    feat: add prefer color theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Типичный веб разработчик</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Используем системную тему в React
-        </a>
-      </header>
+    <div className="app">
+      <span>Hello, World!</span>
     </div>
   );
 }
